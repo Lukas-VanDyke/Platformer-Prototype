9f195515a8eef06c1d8a4d4d74c7e4f0f796a034
Lukas-VanDyke <lukas.vandyke@gmail.com>
2020-01-12 14:29:33 -0700

    Got basic movement (side to side and jumping) working; got gravity working; added test tilemap for floors

--- src/Scripts/PlayerController.gd
+++ src/Scripts/PlayerController.gd
@@ -1,19 +1,23 @@
 extends KinematicBody2D
 
 export (int) var movementSpeed = 300
+export (int) var maxSpeed = 200
 export (int) var climbingSpeed = 100
+export (int) var gravity = 2500
+export (int) var jumpSpeed = -1000
 
 var velocity = Vector2()
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
 func get_input():
-	velocity = Vector2()
+	velocity.x = 0
 	if Input.is_action_pressed("left"):
-		velocity.x = velocity.x - 1
+		velocity.x -= movementSpeed
 	if Input.is_action_pressed("right"):
-		velocity.x = velocity.x + 1
-	
-	velocity = velocity.normalized() * movementSpeed
+		velocity.x += movementSpeed
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		velocity.y = jumpSpeed
