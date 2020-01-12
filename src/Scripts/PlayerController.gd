2de56eb63e64497ca077b46ab7643977a1939970
Lukas-VanDyke <lukas.vandyke@gmail.com>
2020-01-12 12:33:39 -0700

    Reset project to 0 with just empty room, starting from scratch

--- src/Scripts/PlayerController.gd
+++ src/Scripts/PlayerController.gd
@@ -1,21 +1,19 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var movementSpeed = 300
+export (int) var climbingSpeed = 100
 
 var velocity = Vector2()
 
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('right'):
-        velocity.x += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
-
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	get_input()
+	velocity = move_and_slide(velocity)
+	
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("left"):
+		velocity.x = velocity.x - 1
+	if Input.is_action_pressed("right"):
+		velocity.x = velocity.x + 1
+	
+	velocity = velocity.normalized() * movementSpeed
